1ddd454cd9cf12765c687489c24a00e70e7ad333
Vasilij Belosludcev <bupy765@gmail.com>
2016-07-21 23:06:28 +0500

    more code

---
+++ tests/TestTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bupy7\Form\Tests;
+
+use PHPUnit_Framework_TestCase;
+
+class TestTest extends PHPUnit_Framework_TestCase
+{
+    public function testTest()
+    {
+        // OK
+    }
+}
+
