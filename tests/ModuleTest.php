58eee1ac9e2b3a9ed9c964a5d475068c553064f6
Vasilij Belosludcev <bupy765@gmail.com>
2016-07-24 12:41:06 +0500

    more code

---
+++ tests/ModuleTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Bupy7\Form\Tests;
+
+use PHPUnit_Framework_TestCase;
+use Zend\Test\Util\ModuleLoader;
+use Bupy7\Form\Module;
+
+/**
+ * @author Belosludcev Vasilij <https://github.com/bupy7>
+ * @since 1.0.0
+ */
+class ModuleTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * A test of module loader.
+     */
+    public function testLoader()
+    {
+        $moduleLoader = new ModuleLoader([
+            'modules' => [
+                'Bupy7\Form',
+            ],
+            'module_listener_options' => [
+                'module_paths' => [
+                    __DIR__ . '/../src'
+                ],
+                'config_cache_enabled'      => false,
+                'module_map_cache_enabled'  => false,
+                'check_dependencies'        => true,
+            ],
+        ]);
+        $this->assertInstanceOf(Module::class, $moduleLoader->getModule('Bupy7\Form'));
+    }
+}
